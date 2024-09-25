e2e4a13a9bc287f9ce89cd6d133a1f3e5d49df24
Don_israel <israelojakorotu@gmail.com>
2024-09-25 19:58:52 +0100

    Programming 1

---
+++ main.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("Python is a highlevel programming language")
+
+#print a number
+print(10)
+
+#next line after print
+print("this is python \n")
+
+#pass multiple argument for printing
+print("print",5)
+
+#End argument on print statement
+print("welcome to ", end="*")
